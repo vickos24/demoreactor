754a9ccc632abbd68b354d2af07bd0edbf0d9b58
Victor H Portillo R <vickthor24@live.com.mx>
2023-03-08 13:57:21 -0600

    segundo archivo demo

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,12 @@ def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
+def resta(numero1, numero2):
+    total2 = numero1 - numero2
+    return total2
+
 resultado  = suma(num1, num2)
-print('El resultado es : ', resultado)
+resultado2  = resta(num1, num2)
+
+print('El resultado de la suma es : ', resultado)
+print('El resultado de la resta es : ', resultado2)
